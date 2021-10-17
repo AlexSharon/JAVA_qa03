74b3cbd9ce86423f1e7380223fae5efd24eac79e
Alex Sharonov <sharonov-azlk@mail.ru>
2021-10-17 13:29:06 +0300

    hw2 complete

--- Main.java
+++ Main.java
@@ -1,6 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello w");
+
+        long a = 30;
+        long b = 7;
+
+        long r = a + b;
+        System.out.println(r);
+
+        r = a - b;
+        System.out.println(r);
+
+        r = a * b;
+        System.out.println(r);
+
+        r = a / b;
+        System.out.println(r);
+
+        r = a % b;
+        System.out.println(r);
         //comment
+
+
+        r = a % 2;
+        if (r == 0) {
+            System.out.println("variable a: " + a + " is even");
+        }
+        else if (r == 1) {
+            System.out.println("variable a: " + a + " is even");
+        }
+        else {
+            System.out.println("unexpected result:");
+        }
+
+        r = b % 2;
+        if (r == 0) {
+            System.out.println("variable b: " + b + " is even");
+        }
+        else if (r == 1) {
+            System.out.println("variable b: " + b + " is odd");
+        }
+        else {
+            System.out.println("unexpected result:");
+        }
+
+
+        System.out.println("\uD83D\uDE00");
     }
 }
